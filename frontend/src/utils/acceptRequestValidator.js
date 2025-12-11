f46c6ac96f85d89ac126ed922efe4f3bd1e5f7fa
Jainem Patel <jainempatel@Jainams-MacBook-Air.local>
2025-12-11 18:45:41 +0530

    forgot this file for review commit

--- frontend/src/utils/acceptRequestValidator.js
+++ frontend/src/utils/acceptRequestValidator.js
@@ -1,48 +1,47 @@
 import { ErrorToast } from "./Toaster"
 import { GradeValidation, GrNoOrSubIdValidation, StringValidator } from "./validations";
 
-export const adminRequestFieldValidator = (data, toast) => {
+export const adminRequestFieldValidator = (data) => {
     switch (data?.role) {
         case "student":
             if (!GrNoOrSubIdValidation(Number(data?.id))||Number(data?.id)===0) {
-                ErrorToast("invalid gr no/user id provided for student",toast)
-                return
+                ErrorToast("invalid gr no/user id provided for student")
+                return false
             }
             if (!GradeValidation(Number(data?.std))) {
-                ErrorToast("invalid grade/std provided for student",toast)
-                return
+                ErrorToast("invalid grade/std provided for student")
+                return false
             }
             if (!StringValidator(data?.section)) {
-                ErrorToast("invalid section provided for student",toast)
-                return
+                ErrorToast("invalid section provided for student")
+                return false
             }
             break;
         case "teacher":
             if (!GrNoOrSubIdValidation(Number(data?.id))||Number(data?.id)===0) {
-                ErrorToast("invalid gr teacher id assigned",toast)
-                return
+                ErrorToast("invalid gr teacher id assigned")
+                return false
             }
             if (!GrNoOrSubIdValidation(Number(data?.subjectId))||Number(data?.subjectId)===0) {
-                ErrorToast("invalid subject id assigned for teacher",toast)
-                return
+                ErrorToast("invalid subject id assigned for teacher")
+                return false
             }
             if (!GradeValidation(Number(data?.std))) {
-                ErrorToast("invalid grade/std assigned for teacher",toast)
-                return
+                ErrorToast("invalid grade/std assigned for teacher")
+                return false
             }
             if (!StringValidator(data?.section)) {
-                ErrorToast("invalid section assigned for teacher",toast)
-                return
+                ErrorToast("invalid section assigned for teacher")
+                return false
             }
             break;
         case "admin":
             if (!GrNoOrSubIdValidation(Number(data?.id))||Number(data?.id)===0) {
-                ErrorToast("invalid admin id provided",toast)
-                return
+                ErrorToast("invalid admin id provided")
+                return false
             }
             break;
         default:
-            ErrorToast("some error occured. please try again")
-            break;
+            return true
     }
 }
